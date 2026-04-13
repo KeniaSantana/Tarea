ce15834dc9fdd6d5976d0b776b08f0df1dd60a9d
Kenia <23308060610371@cetis61.edu.mx>
2026-04-13 09:01:54 -0600

    x

---
+++ tareas/src/views/loginView.py
@@ -0,0 +1,28 @@
+import flet as flet    
+
+def loginview(page,auth_controlller):
+    email_input=ft.TextField(label="correo electronico",width=350,border_radius=10)
+    pass_input=ft.TextField(label="contraseña",password=True,can_reveal_password=True,width=250,border_radius=10)
+    
+    def login_click(e)
+    user,nsg=auth_controller.login(email_input.value,pass_input.value)
+    if user:
+        page.session.set("user",user)# guarda sesion
+        page.go("/deshboard")
+    else:
+        page.snack_bar=ft.SnackBar(ft.Text(msg))
+        page.snack_bar.open=True
+        page.update()
+        
+    return ft.View("/",[
+        ft.AppBar(title=ft.Text("SIGE-Login"),bgcolor=ft.Colors.BLUE_GREY_900,color="white")
+        ft.Column([
+            ft.Icon(ft.Icons.LOCK_PERSON,size=50,color=ft.Colors.BLUE),
+            ft.Text("Acceso al Sistema",size=24,weight="bold"),
+            email_input,
+            pass_input,
+            ft.ElevateButton("Entrar",on_click=login_click,width=350),
+            ft.TextButton("Crear cuenta nueva",on_click=lambda_:page.go("/registro"))
+        ],horizontal_alignment=ft.CrossAxisAlignment.CENTER,alignment=ft.MainAxisAlignment.CENTER)
+    ])
+        

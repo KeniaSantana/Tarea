21c55a7c4945a1961294b21399857d71337be844
Kenia <23308060610371@cetis61.edu.mx>
2026-04-14 07:30:43 -0600

    x

--- tareas/src/views/main.py
+++ tareas/src/views/main.py
@@ -8,32 +8,19 @@ def start(page: ft.Page):
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
 
-    def route_change(e):
+    def route_change(route):
         page.views.clear()
-
-        # Caso 1: Login
         if page.route == "/":
-            page.add(ft.Text("Caso 1"))
             page.views.append(loginView(page, auth_ctrl))
-
-        # Caso 2: Dashboard
         elif page.route == "/dashboard":
             page.views.append(DashboardView(page, task_ctrl))
-
-        # Caso de seguridad: Si algo falla, mostrar texto de error
-        if not page.views:
-            page.views.append(
-                ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
-            )
 
         page.update()
 
     page.on_route_change = route_change
-    # Forzamos la navegación inicial
     page.go("/")
 
 def main():
-    # Ejecución de la app
     ft.app(target=start)
 
 if __name__ == "__main__":

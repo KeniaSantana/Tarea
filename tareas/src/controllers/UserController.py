59ddccb90fdb4022f6b31464e7fa179262266363
Santana Ruiz Kenia Alejandra <23308060610371@cetis61.edu.mx>
2026-04-21 16:04:09 -0600

    Fix typo in registrar_usuario method and import

--- tareas/src/controllers/UserController.py
+++ tareas/src/controllers/UserController.py
@@ -1,22 +1,17 @@
 from models.UserModel import UsuarioModel
-from models.schemas import UsuarioSchema
+from models.schemasModel import UsuarioSchema
 from pydantic import ValidationError
 
 class AuthController:
     def __init__(self):
         self.model = UsuarioModel()
-
-    def registrar_usuario(self, nombre, email, password):
+        
+    def resgistrar_usuario(self, nombre, email, password):
         try:
-            # Validar datos con el Schema
-            nuevo_usuario = UsuarioSchema(
-                nombre=nombre,
-                email=email,
-                password=password
-            )
+            #validar datos con el schema
+            nuevo_usuario=UsuarioSchema(nombre=nombre, email=email, password=password)
             success = self.model.registrar(nuevo_usuario)
             return success, "Usuario creado correctamente"
-
         except ValidationError as e:
-            # Retorna el primer error de validación encontrado
+            #retorna el primer error de validacion encotrado
             return False, e.errors()[0]['msg']

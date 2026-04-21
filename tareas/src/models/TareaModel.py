48f219a372667b4227e6d597cb0344001760c2d7
Santana Ruiz Kenia Alejandra <23308060610371@cetis61.edu.mx>
2026-04-21 16:05:05 -0600

    Update TareaModel.py

--- tareas/src/models/TareaModel.py
+++ tareas/src/models/TareaModel.py
@@ -2,24 +2,24 @@ from .databaseModel import Database
 
 class TareaModel:
     def __init__(self):
-        self.db = Database()
-    
-    def listar_por_usuario(self,id_usuario):
-        conn=self.db.get_connection()
-        cursor=conn.cursor(dictionary=True)
-        query="SELECT*FROM tareas WHERE id_usuario=%s ORDER BY fecha_limite ASC"
-        cursor.execute(query,(id_usuario,))
-        resultado=cursor.fetchall()
+        self.db=Database()
+        
+    def listar_por_usuario(self, id_usuario):
+        conn = self.deb.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * FROM tareas, WHERE id_usuario = %s ODER BY fecha_limite ASC"
+        cursor.execute(query, (id_usuario))
+        resultado = cursor.fecthall()
         conn.close()
         return resultado
     
-    def crear (self,id_usuario,titulo,descripcion,prioridad,clasificacion):
-        conn=self.db.get_connection()
-        cursor=conn.cursor()
-        query="""INSERT INTO tareas (id_usuario,titulo,descripcion,prioridad,clasificacion) 
-        VALUES (%s,%s,%s,%s,%s)"""
-        cursor.execute(query(id_usuario,titulo,descripcion,prioridad,clasificacion))
+    def crear(self, id_usuario, titulo, descripcion, prioridad, clasificacion):
+        conn = self.db.get_connection()
+        cursor = conn.cursor()
+        query = """INSERT INTO tareas (id_usuario, titulo, descripcion, prioridad, clasificacion)
+                    VALUES (%s, %s, %s, %s, %s)"""
+        cursor.execute(query, (id_usuario, titulo, descripcion, prioridad, clasificacion))
         conn.commit()
-        conn.close
+        conn.close(
     
     
